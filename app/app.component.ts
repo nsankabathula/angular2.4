89552a2a6a71d2e935ac2345175a7943d3c5870c
nsankabathula <naveen.c.s@hotmail.com>
2017-03-08 12:12:00 -0500

    ag-grid 8.2.0

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <button (click) = "videoStart()"> Start Video</button>
+  <video #hardware></video>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Angular'; 
+@ViewChild('hardware') cameraHardware :any;
+
+constructor(){
+
+}
+
+
+videoStart(){
+  let video = this.cameraHardware.nativeElement;
+
+  var nav = <any> navigator;
+  //console.log('nav', nav);
+  nav.getUserMedia = (nav.getUserMedia|| nav.webkitGetUserMedia || nav.mozGetUserMedia || nav.msGetUserMedia);
+  console.log('nav', nav);
+  if(nav.getUserMedia){
+  try{
+  nav.mediaDevices.getUserMedia(
+    {video:true}
+  ).then(function(stream:any){
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  });
+
+  } catch(e){
+    console.error("videoStart error", e);
+  }
+  }
+  else{
+    alert('Browser does not support navigator.getUserMedia.')
+  }
+
+}
+
+}
